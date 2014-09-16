40afa965189aafe59304cdabd1bef54d68dc07c3
Terry Ross <terryross18@yahoo.com>
2014-09-16 18:35:12 -0400

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+// <script type='text/javascript'>
+//   $('.accordion p:nth-child(even)').hide();
+//     $('.accordion p:nth-child(odd)').click(function(){
+//       $('.accordion p:nth-child(even)').hide();
+//         $(this).next().show();
+//     });
+// </script>
