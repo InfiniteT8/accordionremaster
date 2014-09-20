bc528b81aba05bac674b0e666e4d2dd8739c7a0f
Terry Ross <terryross18@yahoo.com>
2014-09-20 00:24:05 -0400

    css update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,11 @@
-// <script type='text/javascript'>
-//   $('.accordion p:nth-child(even)').hide();
-//     $('.accordion p:nth-child(odd)').click(function(){
-//       $('.accordion p:nth-child(even)').hide();
-//         $(this).next().show();
-//     });
-// </script>
+//Old method
+$('.accordion p:nth-child(even)').hide();
+  $('.accordion p:nth-child(odd)').click(function(){
+    $('.accordion p:nth-child(even)').hide();
+      $(this).next().show();
+	});
+
+//Class Method
+// $('.tab').on('click', function () {
+//   $(this).parent().removeClass('hidden').siblings().addClass('hidden');
+// });
